6e909148b1cb6db3f04778ec2c00e2b6dd1d5970
Diego Ploche <diegoploche86@gmail.com>
2023-07-06 23:19:13 -0500

    Creted simple js to lookup a user's information in github using its API. Will return specific error message for 404 responses

---
+++ GET GITHUB USER DATA/index.js
@@ -0,0 +1,34 @@
+const retriveUserInfo = async (user) => {
+    try {
+        let table = document.getElementById('jsonTable'); //get table
+        table.innerHTML = ""; //clear table
+        let errorSpan = document.getElementById('jsonResponse'); //get span to display error message
+        const response = await fetch('https://api.github.com/users/' + user);
+        if (!response.ok) { // check if request failed
+            if(response.status === 404){
+                errorSpan.style.display = 'inline';
+                throw new Error('404: User not found');
+            }
+            throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+            errorSpan.style.display = 'none';
+            const myJson = await response.json(); 
+            for(let i in myJson){
+                let newRow = table.insertRow();
+                let cell1 = newRow.insertCell(0);
+                let cell2 = newRow.insertCell(1);
+                cell1.innerHTML = i;
+                cell2.innerHTML = myJson[i];
+            }
+            document.getElementById('jsonResponse').innerHTML = JSON.stringify(myJson, null, 2);
+        }
+    } catch (error) {
+        console.error('An error occurred while making request:', error);
+        document.getElementById('jsonResponse').innerHTML = error.message;
+    }
+}
+
+function onSearch() {
+    let userName = document.getElementById('userInput').value;
+    retriveUserInfo(userName);
+}
